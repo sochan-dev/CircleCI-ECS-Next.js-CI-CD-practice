8e8dc414d1bef939e194e0ef299df046c26e7ac7
sotatsu kitajima <halosakakitajima@gmail.com>
2022-04-28 16:01:01 +0900

    自動デプロイの設定と表示の変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const AnyComponent = Component as any;
+  //return <AnyComponent {...pageProps} />;
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,32 @@ import type { NextPage } from 'next';
 import { useState, useEffect } from 'react';
 
 type TestExpressMysql = {
-  id: number;
-  name: string;
-}[];
+  msg: string;
+  result: {
+    id: number;
+    name: string;
+  }[];
+};
 const Home: NextPage = () => {
-  console.log('h');
-  console.log('fd');
-  const [testExpressMysql, setTestExpressMysql] = useState<TestExpressMysql>([
-    { id: 0, name: 'まだ' },
-  ]);
+  const [testExpressMysql, setTestExpressMysql] = useState<TestExpressMysql>({
+    msg: 'まだですね。',
+    result: [],
+  });
   useEffect(() => {
-    fetch('http://localhost:4000').then(async (data) => {
-      setTestExpressMysql(await data.json());
-    });
+    fetch('http://ecs-sample-alb-backend-1424607722.ap-northeast-1.elb.amazonaws.com').then(
+      async (data) => {
+        //setTestExpressMysql(data.body);
+        const newData: TestExpressMysql = await data.json();
+        setTestExpressMysql(newData);
+      },
+    );
   }, []);
 
   return (
     <div>
-      {testExpressMysql.map((data, key) => (
+      <h1>Build Nextjs×Express×Mysql service with AWS ECS and CircleCI </h1>
+      <p>{testExpressMysql.msg}</p>
+      {testExpressMysql.result.map((data, key) => (
         <div key={key}>
           <span>{data.id}</span>:<span>{data.name}</span>
         </div>
